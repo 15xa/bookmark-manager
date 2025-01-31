72d67231061d94b018fa5f705136ef03365c5b8f
Aditya <aditya.ps1515@gmail.com>
2025-01-31 16:48:21 +0530

    del

--- src/sections/sec1.jsx
+++
@@ -1,9 +0,0 @@
-import { Navbar } from "../components/navbar"
-
-export const Sec1 = () => {
-    return(
-        <div className="ml-20 mt-20 w-full">
-            <Navbar></Navbar>
-        </div>
-    )
-}
